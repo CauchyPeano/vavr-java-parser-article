f82ec6b6f706bbddca57782f6ff391f65b954594
CauchyPeano <431522+CauchyPeano@users.noreply.github.com>
2019-01-02 17:40:02 +0100

    Initialized project

---
+++ src/main/java/vavr/java/parser/article/App.java
@@ -0,0 +1,11 @@
+package vavr.java.parser.article;
+
+public class App {
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
